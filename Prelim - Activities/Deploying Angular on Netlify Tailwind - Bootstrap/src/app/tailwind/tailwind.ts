e85c97a4073d303fa852090e55384a4ab0546424
Nash-M1 <knashpmallari1@gmail.com>
2025-12-05 23:52:53 +0800

    Bootstrap Tailwind Netlify

---
+++ Prelim - Activities/Deploying Angular on Netlify Tailwind - Bootstrap/src/app/tailwind/tailwind.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tailwind',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tailwind.html',
+  styleUrls: ['./tailwind.css']
+})
+export class Tailwind {}
